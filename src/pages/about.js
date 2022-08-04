e68c0f9a2e6f7f4961a920e29b8d2abaa2b89a53
ayanmta <saiyamchauhan98@gmail.com>
2022-08-04 14:02:50 +0530

    part 2 gatsby completed

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "gatsby";
+import Layout from "./layout";
+
+const about = () => {
+  return (
+    <Layout pageTitle="About Page">
+      <p>hey i am a guy from mountains , love doing things</p>
+    </Layout>
+  );
+};
+
+export default about;
